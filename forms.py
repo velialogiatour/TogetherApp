1d15a3776ff1ba31f80a3fb7133d1f2f85f15359
Данил Азанов <danielsoundwaves@gmail.com>
2025-05-05 22:27:42 +0500

    Добавлены формы

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, ValidationError, EqualTo
+from wtforms import PasswordField, BooleanField, StringField, IntegerField, TextAreaField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, ValidationError, EqualTo, NumberRange
+from flask_wtf.file import FileField, FileAllowed, MultipleFileField
 from models import User
 
 
@@ -45,5 +46,17 @@ class ResetPasswordForm(FlaskForm):
 
     submit = SubmitField('Изменить пароль')
 
-class QuestionForm(FlaskForm):
-    pass
+class QuestionnaireForm(FlaskForm):
+    age = IntegerField('Возраст', validators=[DataRequired(), NumberRange(min=18, max=99)])
+    gender = SelectField('Пол', choices=[('Мужчина', 'Мужчина'), ('Женщина', 'Женщина'), ('Другое', 'Другое')], validators=[DataRequired()])
+    country = StringField('Страна', validators=[DataRequired()])
+    city = StringField('Город', validators=[DataRequired()])
+    height = IntegerField('Рост (см)', validators=[NumberRange(min=100, max=250)])
+    zodiac_sign = StringField('Знак зодиака')
+    interests = TextAreaField('Интересы')
+    description = TextAreaField('О себе')
+
+    profile_photo = FileField('Фото профиля', validators=[FileAllowed(['jpg', 'jpeg', 'png'], 'Только изображения')])
+    additional_photos = MultipleFileField('Дополнительные фото', validators=[FileAllowed(['jpg', 'jpeg', 'png'], 'Только изображения')])
+
+    submit = SubmitField('Создать анкету')
