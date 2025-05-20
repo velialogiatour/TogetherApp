a585f18396c9825c43c1bc2ead8abc4ca388bc7e
Данил Азанов <danielsoundwaves@gmail.com>
2025-05-20 23:02:18 +0500

    Переписан код

--- toxic_filter.py
+++ toxic_filter.py
@@ -3,10 +3,13 @@ import joblib
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
+MODEL_DIR = "ml_model"
 
-MODEL_PATH = os.path.join("ml_model", "toxicity_filter_model.joblib")
+MODEL_PATH = os.path.join(MODEL_DIR, "toxicity_filter_model.joblib")
+VECTORIZER_PATH = os.path.join(MODEL_DIR, "vectorizer.joblib")
 
 model = joblib.load(MODEL_PATH)
+vectorizer = joblib.load(VECTORIZER_PATH)
 
 def is_offensive(text, threshold=0.7):
     """
@@ -15,5 +18,6 @@ def is_offensive(text, threshold=0.7):
     if not text.strip():
         return False
 
-    prob = model.predict_proba([text])[0][1]
+    vector = vectorizer.transform([text])
+    prob = model.predict_proba(vector)[0][1]
     return prob >= threshold
