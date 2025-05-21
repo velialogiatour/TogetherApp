d7bc6dd4141109c714e67ce91b3a36ad72d17504
Данил Азанов <danielsoundwaves@gmail.com>
2025-05-21 22:38:52 +0500

    Обновлена верстка страницы

--- static/js/questionary.js
+++ static/js/questionary.js
@@ -21,6 +21,39 @@ if (flashMessage) {
     flashMessage.style.opacity = '0';
     setTimeout(() => {
       flashMessage.remove();
-    }, 500); // удаляем после исчезновения
-  }, 3000); // ждём 3 секунды
+    }, 500);
+  }, 3000);
+}
+
+// Динамическая подгрузка городов по выбранной стране
+const countrySelect = document.getElementById('country');
+const citySelect = document.getElementById('city');
+
+const citiesByCountry = {
+  "Россия": ["Москва", "Санкт-Петербург", "Новосибирск", "Екатеринбург", "Казань", "Нижний Новгород", "Челябинск", "Самара", "Уфа", "Ростов-на-Дону", "Красноярск", "Пермь", "Воронеж", "Омск", "Тюмень", "Томск", "Барнаул", "Иркутск"],
+  "Беларусь": ["Минск", "Гомель", "Могилёв", "Витебск", "Гродно", "Брест"],
+  "Казахстан": ["Алматы", "Астана", "Шымкент", "Караганда", "Актобе", "Тараз"],
+  "Армения": ["Ереван", "Гюмри", "Ванадзор"],
+  "Азербайджан": ["Баку", "Гянджа", "Сумгаит"],
+  "Кыргызстан": ["Бишкек", "Ош", "Джалал-Абад"],
+  "Таджикистан": ["Душанбе", "Худжанд", "Бохтар"],
+  "Узбекистан": ["Ташкент", "Самарканд", "Бухара", "Наманган", "Андижан"],
+  "Молдова": ["Кишинёв", "Бельцы", "Тирасполь"],
+  "Грузия": ["Тбилиси", "Батуми", "Кутаиси"]
+};
+
+if (countrySelect && citySelect) {
+  countrySelect.addEventListener('change', function () {
+    const selected = this.value;
+    const cities = citiesByCountry[selected] || [];
+
+    citySelect.innerHTML = '<option value="">Выберите город</option>';
+
+    cities.forEach(city => {
+      const option = document.createElement('option');
+      option.value = city;
+      option.textContent = city;
+      citySelect.appendChild(option);
+    });
+  });
 }
