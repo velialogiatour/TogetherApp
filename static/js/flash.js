3820909f87603ce3760e66d5aa40717043d006a4
Данил Азанов <danielsoundwaves@gmail.com>
2025-05-30 02:48:42 +0500

    Исправлено отображение flash уведомления

--- static/js/flash.js
+++ static/js/flash.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Обработка старого формата .flash
     const flashes = document.querySelectorAll(".flash");
 
     flashes.forEach(flash => {
@@ -7,6 +8,19 @@ document.addEventListener("DOMContentLoaded", function () {
             flash.style.transform = "translateY(-20px)";
             setTimeout(() => {
                 flash.style.display = "none";
+            }, 500);
+        }, 3000);
+    });
+
+    // Обработка новых уведомлений в .toast
+    const toasts = document.querySelectorAll(".toast");
+
+    toasts.forEach(toast => {
+        setTimeout(() => {
+            toast.style.opacity = "0";
+            toast.style.transform = "translateY(-20px)";
+            setTimeout(() => {
+                toast.remove();
             }, 500);
         }, 3000);
     });
